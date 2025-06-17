62c4cee11b1dbb82b798c071a82308dc939925b8
lungile sibanda <ST10476414@rcconnect.edu.za>
2025-06-17 19:42:08 +0200

    POE

---
+++ src/main/java/com/mycompany/messaging_app_part1/Login.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.messaging_app_part1;
+
+/**
+ *
+ * @author User
+ */
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Login {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String cellPhone;
+
+    public Login(String username, String password, String firstName, String lastName, String cellPhone) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cellPhone = cellPhone;
+    }
+
+    public boolean checkUserName() {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity() {
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\\\"\\\\|,.<>\\/?]).{8,}$");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public boolean checkCellPhoneNumber() {
+        Pattern pattern = Pattern.compile("^\\+27\\d{9}$");
+        Matcher matcher = pattern.matcher(cellPhone);
+        return matcher.matches();
+    }
+
+    public String registerUser() {
+        if (!checkUserName()) {
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+        if (!checkPasswordComplexity()) {
+            return "Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+        if (!checkCellPhoneNumber()) {
+            return "Cell number is incorrectly formatted or does not contain an international code, please correct the number and try again.";
+        }
+        return "Username successfully captured.\nPassword successfully captured.\nCell phone number successfully added.";
+    }
+
+    public boolean loginUser(String inputUsername, String inputPassword) {
+        return this.username.equals(inputUsername) && this.password.equals(inputPassword);
+    }
+
+    public String returnLoginStatus(String inputUsername, String inputPassword) {
+        if (loginUser(inputUsername, inputPassword)) {
+            return "Welcome " + firstName + " " + lastName + ", it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+            
+        }
+    }
+}
